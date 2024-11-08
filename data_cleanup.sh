b30e9f95e3904b97b805350d1db9428aa1c87254
Richard Dunks <richarddunks@gmail.com>
2024-11-08 15:26:58 -0600

    initial commit of cleanup processing file

---
+++ data_cleanup.sh
@@ -0,0 +1,5 @@
+# Commands to cut down and cleanup the MSP datafile
+# downloaded from https://www.mspb.gov/foia/files/msp2021/MSPB_MPS2021_Release-CSV.zip
+
+# create a 100 line sample of the file with all columns for testing
+head -n 100 MSPB_MPS2021_Release.CSV >> MSPB_MPS2021_Release_100.csv
