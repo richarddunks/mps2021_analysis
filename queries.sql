c668e5780c30f31ecc5b332a246edecce05c095c
Richard Dunks <richarddunks@gmail.com>
2024-12-11 22:44:06 -0600

    fixing the sql queries

--- queries.sql
+++ queries.sql
@@ -1,32 +1,29 @@
 -- queries for each of the bivariate analysis
 
--- Intent to turnover measures
+-- Intent to turnover bivariate
 SELECT
   mps_weight,
 	(i_prov_nec_train::DECIMAL + i_prov_opp_growth::DECIMAL + i_given_opp_skills::DECIMAL)/3 as i_average, 
-	(d_meaningful_work::DECIMAL + d_move_diff_occ::DECIMAL)/2 as d_average
+	d_resign_gov::int
 FROM mps_training 
 WHERE c_retire_elg = '0' 
-	-- independent variables
 	AND (i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
 	AND (i_prov_opp_growth != 'D' AND i_prov_opp_growth IS NOT NULL) 
 	AND (i_given_opp_skills != 'D' AND i_given_opp_skills IS NOT NULL)
-	-- ITT dependent variables
-	AND (d_resign_gov != 'D' AND d_resign_gov IS NOT NULL)
-  	AND (d_move_diff_occ != 'D' AND d_move_diff_occ IS NOT NULL);
+	AND (d_resign_gov != 'D' AND d_resign_gov IS NOT NULL);
 
--- Personal Job Satisfaction
+-- Personal Job Satisfaction bivariate
 SELECT
   mps_weight,
 	(i_prov_nec_train::DECIMAL + i_prov_opp_growth::DECIMAL + i_given_opp_skills::DECIMAL)/3 as i_average,
 	(d_meaningful_work::DECIMAL + d_recommend_agency::DECIMAL + d_use_job_skills::DECIMAL)/3 as d_average
 FROM mps_training 
-WHERE c_retire_elg = '0' 
+WHERE 
 	-- independent variables
-	AND (i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
+	(i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
 	AND (i_prov_opp_growth != 'D' AND i_prov_opp_growth IS NOT NULL) 
 	AND (i_given_opp_skills != 'D' AND i_given_opp_skills IS NOT NULL)
-	-- PJS dependent variables
+	-- dependent variables
 	AND (d_meaningful_work != 'D' AND d_meaningful_work IS NOT NULL)
 	AND (d_recommend_agency != 'D' AND d_recommend_agency IS NOT NULL)
 	AND (d_use_job_skills != 'D' AND d_use_job_skills IS NOT NULL);
@@ -37,9 +34,8 @@ SELECT
 	(i_prov_nec_train::DECIMAL + i_prov_opp_growth::DECIMAL + i_given_opp_skills::DECIMAL)/3 as i_average,
 	(d_take_new_roles::DECIMAL + d_take_high_tech_resp::DECIMAL + d_take_super_resp::DECIMAL)/3 as d_average
 FROM mps_training 
-WHERE c_retire_elg = '0' 
-	-- independent variables
-	AND (i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
+WHERE 
+  (i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
 	AND (i_prov_opp_growth != 'D' AND i_prov_opp_growth IS NOT NULL) 
 	AND (i_given_opp_skills != 'D' AND i_given_opp_skills IS NOT NULL)
 	-- dependent variables
@@ -53,9 +49,9 @@ SELECT
 	(i_prov_nec_train::DECIMAL + i_prov_opp_growth::DECIMAL + i_given_opp_skills::DECIMAL)/3 as i_average,
 	(d_unit_high_qual_prod::DECIMAL + d_use_wf_eff::DECIMAL + d_retain_best::DECIMAL)/3 as d_average 
 FROM mps_training
-WHERE c_retire_elg = '0' 
+WHERE 
 	-- independent variables
-	AND (i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
+	(i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
 	AND (i_prov_opp_growth != 'D' AND i_prov_opp_growth IS NOT NULL) 
 	AND (i_given_opp_skills != 'D' AND i_given_opp_skills IS NOT NULL)
 	-- dependent variables
@@ -117,3 +113,175 @@ WHERE c_retire_elg = '0'
 	AND (d_unit_high_qual_prod != 'D' AND d_unit_high_qual_prod IS NOT NULL)
 	AND (d_use_wf_eff != 'D' AND d_use_wf_eff IS NOT NULL)
 	AND (d_retain_best != 'D' AND d_retain_best IS NOT NULL);
+
+-- ITT multivariate
+SELECT
+	mps_weight,
+	MPS_Agency,
+	-- dummy variable for supervisor or executive
+	CASE WHEN MPS_Supervisor = 'S' OR MPS_Supervisor = 'X' THEN 1
+		ELSE 0 END AS mps_supervisor_dummy,
+	-- gender dummy variable
+	CASE WHEN c_gender = '1' THEN 1
+		WHEN c_gender = '2' THEN 0
+		ELSE NULL END AS is_female,
+	c_race_eth::int AS is_minority,
+	-- dummy variable for age 39 or less
+	CASE 
+		WHEN c_age = '4' THEN 0
+		WHEN c_age = '1' THEN 1 
+		ELSE NULL END AS age_39_less,
+	-- dummy variable for education level 
+	CASE WHEN c_ed_level = '5' THEN 1
+		WHEN c_ed_level = '3' THEN 0
+		WHEN c_ed_level = '1' THEN 0
+		ELSE NULL END AS has_masters_plus,
+	c_fair_org_pay::int,
+	(i_prov_nec_train::DECIMAL + i_prov_opp_growth::DECIMAL + i_given_opp_skills::DECIMAL)/3 as i_average, 
+	d_resign_gov::INT
+FROM mps_training 
+WHERE c_retire_elg = '0' 
+	-- control variables
+	AND mps_supervisor IS NOT NULL
+	AND c_gender IS NOT NULL
+	AND c_race_eth IS NOT NULL
+	AND c_age IS NOT NULL
+	AND c_ed_level IS NOT NULL
+	AND (c_fair_org_pay	 != 'D' AND c_fair_org_pay IS NOT NULL)
+	-- independent variables
+	AND (i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
+	AND (i_prov_opp_growth != 'D' AND i_prov_opp_growth IS NOT NULL) 
+	AND (i_given_opp_skills != 'D' AND i_given_opp_skills IS NOT NULL)
+	-- ITT dependent variables
+	AND (d_resign_gov != 'D' AND d_resign_gov IS NOT NULL);
+
+-- PJS multivariate
+SELECT
+	mps_weight,
+	MPS_Agency,
+	-- dummy variable for supervisor or executive
+	CASE WHEN MPS_Supervisor = 'S' OR MPS_Supervisor = 'X' THEN 1
+		ELSE 0 END AS mps_supervisor_dummy,
+	-- gender dummy variable
+	CASE WHEN c_gender = '1' THEN 1
+		WHEN c_gender = '2' THEN 0
+		ELSE NULL END AS is_female,
+	c_race_eth::int AS is_minority,
+	-- dummy variable for age 39 or less
+	CASE 
+		WHEN c_age = '4' THEN 0
+		WHEN c_age = '1' THEN 1 
+		ELSE NULL END AS age_39_less,
+	-- dummy variable for education level
+	CASE WHEN c_ed_level = '5' THEN 1
+		WHEN c_ed_level = '3' THEN 0
+		WHEN c_ed_level = '1' THEN 0
+		ELSE NULL END AS has_masters_plus,
+	c_fair_org_pay::int,
+	(i_prov_nec_train::DECIMAL + i_prov_opp_growth::DECIMAL + i_given_opp_skills::DECIMAL)/3 as i_average, 
+	(d_meaningful_work::DECIMAL + d_recommend_agency::DECIMAL + d_use_job_skills::DECIMAL)/3 as d_pjs_avg
+FROM mps_training 
+WHERE 
+	-- control variables
+	mps_supervisor IS NOT NULL
+	AND c_gender IS NOT NULL
+	AND c_race_eth IS NOT NULL
+	AND c_age IS NOT NULL
+	AND c_ed_level IS NOT NULL
+	AND (c_fair_org_pay	 != 'D' AND c_fair_org_pay IS NOT NULL)
+	-- independent variables
+	AND (i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
+	AND (i_prov_opp_growth != 'D' AND i_prov_opp_growth IS NOT NULL) 
+	AND (i_given_opp_skills != 'D' AND i_given_opp_skills IS NOT NULL)
+	-- PJS dependent variables
+	AND (d_meaningful_work != 'D' AND d_meaningful_work IS NOT NULL)
+	AND (d_recommend_agency != 'D' AND d_recommend_agency IS NOT NULL)
+	AND (d_use_job_skills != 'D' AND d_use_job_skills IS NOT NULL)
+;
+-- PGO multivariate
+SELECT
+	mps_weight,
+	MPS_Agency,
+	-- dummy variable for supervisor or executive
+	CASE WHEN MPS_Supervisor = 'S' OR MPS_Supervisor = 'X' THEN 1
+		ELSE 0 END AS mps_supervisor_dummy,
+	-- gender dummy variable
+	CASE WHEN c_gender = '1' THEN 1
+		WHEN c_gender = '2' THEN 0
+		ELSE NULL END AS is_female,
+	c_race_eth::int AS is_minority,
+	-- dummy variable for age 39 or less
+	CASE 
+		WHEN c_age = '4' THEN 0
+		WHEN c_age = '1' THEN 1 
+		ELSE NULL END AS age_39_less,
+	-- dummy variable for education level
+	CASE WHEN c_ed_level = '5' THEN 1
+		WHEN c_ed_level = '3' THEN 0
+		WHEN c_ed_level = '1' THEN 0
+		ELSE NULL END AS has_masters_plus,
+	c_fair_org_pay::int,
+	(i_prov_nec_train::DECIMAL + i_prov_opp_growth::DECIMAL + i_given_opp_skills::DECIMAL)/3 as i_average, 
+	(d_take_new_roles::DECIMAL + d_take_high_tech_resp::DECIMAL + d_take_super_resp::DECIMAL)/3 as d_pgo_avg
+FROM mps_training 
+WHERE 
+	-- control variables
+	mps_supervisor IS NOT NULL
+	AND c_gender IS NOT NULL
+	AND c_race_eth IS NOT NULL
+	AND c_age IS NOT NULL
+	AND c_ed_level IS NOT NULL
+	AND (c_fair_org_pay	 != 'D' AND c_fair_org_pay IS NOT NULL)
+	-- independent variables
+	AND (i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
+	AND (i_prov_opp_growth != 'D' AND i_prov_opp_growth IS NOT NULL) 
+	AND (i_given_opp_skills != 'D' AND i_given_opp_skills IS NOT NULL)
+	-- PGO dependent variables
+	AND (d_take_new_roles != 'D' AND d_take_new_roles IS NOT NULL)
+	AND (d_take_high_tech_resp != 'D' AND d_take_high_tech_resp IS NOT NULL)
+	AND (d_take_super_resp != 'D' AND d_take_super_resp IS NOT NULL)
+;
+
+-- AS multivariate
+SELECT
+	mps_weight,
+	MPS_Agency,
+	-- dummy variable for supervisor or executive
+	CASE WHEN MPS_Supervisor = 'S' OR MPS_Supervisor = 'X' THEN 1
+		ELSE 0 END AS mps_supervisor_dummy,
+	-- gender dummy variable
+	CASE WHEN c_gender = '1' THEN 1
+		WHEN c_gender = '2' THEN 0
+		ELSE NULL END AS is_female,
+	c_race_eth::int AS is_minority,
+	-- dummy variable for age 39 or less
+	CASE 
+		WHEN c_age = '4' THEN 0
+		WHEN c_age = '1' THEN 1 
+		ELSE NULL END AS age_39_less,
+	-- dummy variable for education level
+	CASE WHEN c_ed_level = '5' THEN 1
+		WHEN c_ed_level = '3' THEN 0
+		WHEN c_ed_level = '1' THEN 0
+		ELSE NULL END AS has_masters_plus,
+	c_fair_org_pay::int,
+	(i_prov_nec_train::DECIMAL + i_prov_opp_growth::DECIMAL + i_given_opp_skills::DECIMAL)/3 as i_average, 
+	(d_unit_high_qual_prod::DECIMAL + d_use_wf_eff::DECIMAL + d_retain_best::DECIMAL)/3 as d_as_average
+FROM mps_training 
+WHERE 
+	-- control variables
+	mps_supervisor IS NOT NULL
+	AND c_gender IS NOT NULL
+	AND c_race_eth IS NOT NULL
+	AND c_age IS NOT NULL
+	AND c_ed_level IS NOT NULL
+	AND (c_fair_org_pay	 != 'D' AND c_fair_org_pay IS NOT NULL)
+	-- independent variables
+	AND (i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
+	AND (i_prov_opp_growth != 'D' AND i_prov_opp_growth IS NOT NULL) 
+	AND (i_given_opp_skills != 'D' AND i_given_opp_skills IS NOT NULL)
+	-- AS dependent variables
+	AND (d_unit_high_qual_prod != 'D' AND d_unit_high_qual_prod IS NOT NULL)
+	AND (d_use_wf_eff != 'D' AND d_use_wf_eff IS NOT NULL)
+	AND (d_retain_best != 'D' AND d_retain_best IS NOT NULL)
+;
