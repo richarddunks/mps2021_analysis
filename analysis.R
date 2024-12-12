383f3786378b92e085ad86d0eabac3a21e760f3a
Richard Dunks <richarddunks@gmail.com>
2024-12-11 22:59:14 -0600

    refactored and working for stargazer

--- analysis.R
+++ analysis.R
@@ -193,8 +193,7 @@ sink()
 # install.packages("stargazer")
 library("stargazer")
 
-## ITT model
-itt_query <- "SELECT
+query <- "SELECT
 	mps_weight,
 	MPS_Agency,
 	-- dummy variable for supervisor or executive
@@ -214,192 +213,106 @@ itt_query <- "SELECT
 		WHEN c_ed_level = '1' THEN 0
 		ELSE NULL END AS has_masters_plus,
 	c_fair_org_pay::int,
-	(i_prov_nec_train::DECIMAL + i_prov_opp_growth::DECIMAL + i_given_opp_skills::DECIMAL)/3 as i_average, 
-	d_resign_gov::INT
+-- independent variables
+	CASE 
+		WHEN 
+			i_prov_nec_train != 'D' 
+			AND i_prov_nec_train IS NOT NULL 
+			AND i_prov_opp_growth != 'D' 
+			AND i_prov_opp_growth IS NOT NULL 
+			AND i_given_opp_skills != 'D' 
+			AND i_given_opp_skills IS NOT NULL 
+		THEN (i_prov_nec_train::DECIMAL + i_prov_opp_growth::DECIMAL + i_given_opp_skills::DECIMAL)/3
+		ELSE NULL 
+			END AS i_average,
+-- itt dependent variable
+	CASE
+		WHEN 
+			d_resign_gov != 'D' 
+			AND d_resign_gov IS NOT NULL
+			AND c_retire_elg = '0'
+		THEN d_resign_gov::INT
+		ELSE NULL
+			END AS d_resign_gov,
+-- pjs dependent variables
+	CASE 
+		WHEN 
+			d_meaningful_work != 'D' 
+			AND d_meaningful_work IS NOT NULL
+			AND d_recommend_agency != 'D' 
+			AND d_recommend_agency IS NOT NULL
+			AND d_use_job_skills != 'D' 
+			AND d_use_job_skills IS NOT NULL
+		THEN (d_meaningful_work::DECIMAL + d_recommend_agency::DECIMAL + d_use_job_skills::DECIMAL)/3 
+		ELSE NULL 
+			END AS d_pjs_avg,
+-- pgo dependent variables
+	CASE 
+		WHEN 
+			d_take_new_roles != 'D' 
+			AND d_take_new_roles IS NOT NULL
+			AND d_take_high_tech_resp != 'D' 
+			AND d_take_high_tech_resp IS NOT NULL
+			AND d_take_super_resp != 'D' 
+			AND d_take_super_resp IS NOT NULL
+		THEN (d_take_new_roles::DECIMAL + d_take_high_tech_resp::DECIMAL + d_take_super_resp::DECIMAL)/3
+		ELSE NULL 
+			END AS d_pgo_avg,
+-- as dependent variables
+	CASE 
+		WHEN
+			d_unit_high_qual_prod != 'D' 
+			AND d_unit_high_qual_prod IS NOT NULL
+			AND d_use_wf_eff != 'D' 
+			AND d_use_wf_eff IS NOT NULL
+			AND d_retain_best != 'D' 
+			AND d_retain_best IS NOT NULL
+		THEN (d_unit_high_qual_prod::DECIMAL + d_use_wf_eff::DECIMAL + d_retain_best::DECIMAL)/3 
+		ELSE NULL 
+			END AS d_as_average 
 FROM mps_training 
-WHERE c_retire_elg = '0' 
+WHERE  
 	-- control variables
-	AND mps_supervisor IS NOT NULL
+	mps_supervisor IS NOT NULL
 	AND c_gender IS NOT NULL
 	AND c_race_eth IS NOT NULL
 	AND c_age IS NOT NULL
 	AND c_ed_level IS NOT NULL
-	AND (c_fair_org_pay	 != 'D' AND c_fair_org_pay IS NOT NULL)
-	-- independent variables
-	AND (i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
-	AND (i_prov_opp_growth != 'D' AND i_prov_opp_growth IS NOT NULL) 
-	AND (i_given_opp_skills != 'D' AND i_given_opp_skills IS NOT NULL)
-	-- ITT dependent variables
-	AND (d_resign_gov != 'D' AND d_resign_gov IS NOT NULL);"
+	AND (c_fair_org_pay	 != 'D' AND c_fair_org_pay IS NOT NULL);"
 
 # execute query and load the result as a dataframe for analysis
-itt_result <- dbGetQuery(con,itt_query)
+result <- dbGetQuery(con,query)
 
 # model building (ITT)
-itt_model <- lm(itt_result$d_resign_gov ~ itt_result$i_average + itt_result$mps_supervisor_dummy + itt_result$is_female + itt_result$is_minority + itt_result$age_39_less + itt_result$has_masters_plus + itt_result$c_fair_org_pay, weights = itt_result$mps_weight)
+itt_model <- lm(result$d_resign_gov ~ result$i_average + result$mps_supervisor_dummy + result$is_female + result$is_minority + result$age_39_less + result$has_masters_plus + result$c_fair_org_pay, weights = result$mps_weight)
 sink("multi_itt_results.txt")
 print(summary(itt_model))
 sink()
 
 # model building (PJS)
-pjs_query <- "SELECT
-	mps_weight,
-	MPS_Agency,
-	-- dummy variable for supervisor or executive
-	CASE WHEN MPS_Supervisor = 'S' OR MPS_Supervisor = 'X' THEN 1
-		ELSE 0 END AS mps_supervisor_dummy,
-	-- gender dummy variable
-	CASE WHEN c_gender = '1' THEN 1
-		WHEN c_gender = '2' THEN 0
-		ELSE NULL END AS is_female,
-	c_race_eth::int AS is_minority,
-	-- dummy variable for age 39 or less
-	CASE 
-		WHEN c_age = '4' THEN 0
-		WHEN c_age = '1' THEN 1 
-		ELSE NULL END AS age_39_less,
-	-- dummy variable for education level
-	CASE WHEN c_ed_level = '5' THEN 1
-		WHEN c_ed_level = '3' THEN 0
-		WHEN c_ed_level = '1' THEN 0
-		ELSE NULL END AS has_masters_plus,
-	c_fair_org_pay::int,
-	(i_prov_nec_train::DECIMAL + i_prov_opp_growth::DECIMAL + i_given_opp_skills::DECIMAL)/3 as i_average, 
-	(d_meaningful_work::DECIMAL + d_recommend_agency::DECIMAL + d_use_job_skills::DECIMAL)/3 as d_pjs_avg
-FROM mps_training 
-WHERE 
-	-- control variables
-	mps_supervisor IS NOT NULL
-	AND c_gender IS NOT NULL
-	AND c_race_eth IS NOT NULL
-	AND c_age IS NOT NULL
-	AND c_ed_level IS NOT NULL
-	AND (c_fair_org_pay	 != 'D' AND c_fair_org_pay IS NOT NULL)
-	-- independent variables
-	AND (i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
-	AND (i_prov_opp_growth != 'D' AND i_prov_opp_growth IS NOT NULL) 
-	AND (i_given_opp_skills != 'D' AND i_given_opp_skills IS NOT NULL)
-	-- PJS dependent variables
-	AND (d_meaningful_work != 'D' AND d_meaningful_work IS NOT NULL)
-	AND (d_recommend_agency != 'D' AND d_recommend_agency IS NOT NULL)
-	AND (d_use_job_skills != 'D' AND d_use_job_skills IS NOT NULL)
-;"
-
-# execute query and load the result as a dataframe for analysis
-pjs_result <- dbGetQuery(con,pjs_query)
 
 
-pjs_model <- lm(pjs_result$d_pjs_avg ~ pjs_result$i_average + pjs_result$mps_supervisor_dummy + pjs_result$is_female + pjs_result$is_minority + pjs_result$age_39_less + pjs_result$has_masters_plus + pjs_result$c_fair_org_pay, weights = pjs_result$mps_weight)
+pjs_model <- lm(result$d_pjs_avg ~ result$i_average + result$mps_supervisor_dummy + result$is_female + result$is_minority + result$age_39_less + result$has_masters_plus + result$c_fair_org_pay, weights = result$mps_weight)
 sink("multi_pjs_results.txt")
 print(summary(pjs_model))
 sink()
 
 # model building (PGO)
-pgo_query <- "SELECT
-	mps_weight,
-	MPS_Agency,
-	-- dummy variable for supervisor or executive
-	CASE WHEN MPS_Supervisor = 'S' OR MPS_Supervisor = 'X' THEN 1
-		ELSE 0 END AS mps_supervisor_dummy,
-	-- gender dummy variable
-	CASE WHEN c_gender = '1' THEN 1
-		WHEN c_gender = '2' THEN 0
-		ELSE NULL END AS is_female,
-	c_race_eth::int AS is_minority,
-	-- dummy variable for age 39 or less
-	CASE 
-		WHEN c_age = '4' THEN 0
-		WHEN c_age = '1' THEN 1 
-		ELSE NULL END AS age_39_less,
-	-- dummy variable for education level
-	CASE WHEN c_ed_level = '5' THEN 1
-		WHEN c_ed_level = '3' THEN 0
-		WHEN c_ed_level = '1' THEN 0
-		ELSE NULL END AS has_masters_plus,
-	c_fair_org_pay::int,
-	(i_prov_nec_train::DECIMAL + i_prov_opp_growth::DECIMAL + i_given_opp_skills::DECIMAL)/3 as i_average, 
-	(d_take_new_roles::DECIMAL + d_take_high_tech_resp::DECIMAL + d_take_super_resp::DECIMAL)/3 as d_pgo_avg
-FROM mps_training 
-WHERE 
-	-- control variables
-	mps_supervisor IS NOT NULL
-	AND c_gender IS NOT NULL
-	AND c_race_eth IS NOT NULL
-	AND c_age IS NOT NULL
-	AND c_ed_level IS NOT NULL
-	AND (c_fair_org_pay	 != 'D' AND c_fair_org_pay IS NOT NULL)
-	-- independent variables
-	AND (i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
-	AND (i_prov_opp_growth != 'D' AND i_prov_opp_growth IS NOT NULL) 
-	AND (i_given_opp_skills != 'D' AND i_given_opp_skills IS NOT NULL)
-	-- PGO dependent variables
-	AND (d_take_new_roles != 'D' AND d_take_new_roles IS NOT NULL)
-	AND (d_take_high_tech_resp != 'D' AND d_take_high_tech_resp IS NOT NULL)
-	AND (d_take_super_resp != 'D' AND d_take_super_resp IS NOT NULL)
-;"
 
-# execute query and load the result as a dataframe for analysis
-pgo_result <- dbGetQuery(con,pgo_query)
-
-pgo_model <- lm(pgo_result$d_pgo_avg ~ pgo_result$i_average + pgo_result$mps_supervisor_dummy + pgo_result$is_female + pgo_result$is_minority + pgo_result$age_39_less + pgo_result$has_masters_plus + pgo_result$c_fair_org_pay, weights = pgo_result$mps_weight)
+pgo_model <- lm(result$d_pgo_avg ~ result$i_average + result$mps_supervisor_dummy + result$is_female + result$is_minority + result$age_39_less + result$has_masters_plus + result$c_fair_org_pay, weights = result$mps_weight)
 sink("multi_pgo_results.txt")
 print(summary(pgo_model))
 sink()
 
 # model building (AS)
-as_query <- "SELECT
-	mps_weight,
-	MPS_Agency,
-	-- dummy variable for supervisor or executive
-	CASE WHEN MPS_Supervisor = 'S' OR MPS_Supervisor = 'X' THEN 1
-		ELSE 0 END AS mps_supervisor_dummy,
-	-- gender dummy variable
-	CASE WHEN c_gender = '1' THEN 1
-		WHEN c_gender = '2' THEN 0
-		ELSE NULL END AS is_female,
-	c_race_eth::int AS is_minority,
-	-- dummy variable for age 39 or less
-	CASE 
-		WHEN c_age = '4' THEN 0
-		WHEN c_age = '1' THEN 1 
-		ELSE NULL END AS age_39_less,
-	-- dummy variable for education level
-	CASE WHEN c_ed_level = '5' THEN 1
-		WHEN c_ed_level = '3' THEN 0
-		WHEN c_ed_level = '1' THEN 0
-		ELSE NULL END AS has_masters_plus,
-	c_fair_org_pay::int,
-	(i_prov_nec_train::DECIMAL + i_prov_opp_growth::DECIMAL + i_given_opp_skills::DECIMAL)/3 as i_average, 
-	(d_unit_high_qual_prod::DECIMAL + d_use_wf_eff::DECIMAL + d_retain_best::DECIMAL)/3 as d_as_average
-FROM mps_training 
-WHERE 
-	-- control variables
-	mps_supervisor IS NOT NULL
-	AND c_gender IS NOT NULL
-	AND c_race_eth IS NOT NULL
-	AND c_age IS NOT NULL
-	AND c_ed_level IS NOT NULL
-	AND (c_fair_org_pay	 != 'D' AND c_fair_org_pay IS NOT NULL)
-	-- independent variables
-	AND (i_prov_nec_train != 'D' AND i_prov_nec_train IS NOT NULL)
-	AND (i_prov_opp_growth != 'D' AND i_prov_opp_growth IS NOT NULL) 
-	AND (i_given_opp_skills != 'D' AND i_given_opp_skills IS NOT NULL)
-	-- AS dependent variables
-	AND (d_unit_high_qual_prod != 'D' AND d_unit_high_qual_prod IS NOT NULL)
-	AND (d_use_wf_eff != 'D' AND d_use_wf_eff IS NOT NULL)
-	AND (d_retain_best != 'D' AND d_retain_best IS NOT NULL)
-;"
 
-# execute query and load the result as a dataframe for analysis
-as_result <- dbGetQuery(con,as_query)
-
-as_model <- lm(as_result$d_as_average ~ as_result$i_average + as_result$mps_supervisor_dummy + as_result$is_female + as_result$is_minority + as_result$age_39_less + as_result$has_masters_plus + as_result$c_fair_org_pay, weights = as_result$mps_weight)
+as_model <- lm(result$d_as_average ~ result$i_average + result$mps_supervisor_dummy + result$is_female + result$is_minority + result$age_39_less + result$has_masters_plus + result$c_fair_org_pay, weights = result$mps_weight)
 sink("multi_as_results.txt")
 print(summary(as_model))
 sink()
 
 ## output results with stargazer
-stargazer(itt_model,pjs_model,pgo_model,as_model,type="html",summary=TRUE,out= "multi_model_out.htm",multicolumn = FALSE)
+stargazer(itt_model,pjs_model,pgo_model,as_model,type="html",summary=TRUE,out= "multi_model_out.htm")
 
 ### Additional analysis
 
