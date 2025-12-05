4c32756982102798c2cff3b413bd31340ab7b327
Richard Dunks <richarddunks@gmail.com>
2025-12-05 12:36:30 -0600

    content towards #2

--- analysis.R
+++ analysis.R
@@ -379,11 +379,64 @@ library(semTools)
 library(sqlhelper)
 
 # query string
-query <- sqlhelper::read_sql("")
+query <- "SELECT
+-- control variables
+	MPS_Agency,
+	MPS_Stratum,
+	MPS_Weight,
+	MPS_Supervisor,		
+	DEM_04R AS c_retire_elg,
+	DEM_10R AS c_gender,
+	DEM_ERI AS c_race_eth,
+	DEM_15R AS c_age,
+	DEM_16R AS c_ed_level,
+	MSP_18 AS c_fair_org_pay,
+-- independent variables
+	MSP_10 AS i_prov_nec_train,
+	MSP_11 AS i_prov_opp_growth,
+	ENG_13 AS i_given_opp_skills,
+-- dependent variables
+-- personal job satisfaction
+	ENG_02 AS d_meaningful_work,
+	ENG_04 AS d_recommend_agency,
+	ENG_08 AS d_use_job_skills,
+-- agency satisfaction
+	ENG_03 AS d_unit_high_qual_prod,
+	MSP_01 AS d_use_wf_eff,
+	MSP_07 AS d_retain_best,
+-- professional growth opportunities
+	DEM_05b AS d_take_new_roles,
+	DEM_05c AS d_take_high_tech_resp,
+	DEM_05d AS d_take_super_resp,
+-- intent to turnover
+	DEM_05f AS d_move_diff_occ,
+	DEM_05h AS d_resign_gov
+FROM mps2021; "
 
 
 # execute query and load the result as a dataframe for analysis
 result <- dbGetQuery(con,query)
+
+# personal job satisfaction construct
+pjs_items <- c(
+  result$d_meaningful_work,
+  result$d_recommend_agency,
+  result$d_use_job_skills
+)
+
+# professional growth opportunities construct
+pgo_items <- c(
+  result$d_take_new_roles,
+  result$d_take_high_tech_resp,
+  result$d_take_super_resp
+)
+
+# agency satisfaction
+as_items <- c(
+  result$d_unit_high_qual_prod,
+  result$d_use_wf_eff,
+  result$d_retain_best
+)
 
 # check correlations of weights to variables of interest
 # check weighted and unweighted descriptive estimates, check with standard errors
