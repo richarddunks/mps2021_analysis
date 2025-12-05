c55cbf2afd7733bd4ce1ebbddfccf98ba8d74651
Richard Dunks <richarddunks@gmail.com>
2025-12-05 12:16:54 -0600

    adding comments for clarity

--- create_view_mps_training.sql
+++ create_view_mps_training.sql
@@ -18,15 +18,19 @@ CREATE VIEW mps_training AS
 		MSP_11 AS i_prov_opp_growth,
 		ENG_13 AS i_given_opp_skills,
 	-- dependent variables
+	-- personal job satisfaction
 		ENG_02 AS d_meaningful_work,
 		ENG_04 AS d_recommend_agency,
 		ENG_08 AS d_use_job_skills,
+	-- agency satisfaction
 		ENG_03 AS d_unit_high_qual_prod,
 		MSP_01 AS d_use_wf_eff,
 		MSP_07 AS d_retain_best,
+	-- professional growth opportunities
 		DEM_05b AS d_take_new_roles,
 		DEM_05c AS d_take_high_tech_resp,
 		DEM_05d AS d_take_super_resp,
+	-- intent to turnover
 		DEM_05f AS d_move_diff_occ,
 		DEM_05h AS d_resign_gov
 	FROM mps2021; 
